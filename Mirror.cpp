3eda8fa92f061f66d03e430aa28a367716303fe9
Andrew <andevil95@mail.ru>
2024-09-30 22:43:03 +0300

    I have made a programm, which change sides in .bmp picture

---
+++ Mirror.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void Help(const char* programName);
+std::string Flip_line(const std::string line);
+
+
+int main(int argc, char** argv)
+{
+    const int minProgrammArgsCount = 3;
+    int num_line = 1;
+    if (argc != minProgrammArgsCount)
+    {
+        std::cerr << "Incorrect using!\n";
+        Help(argv[0]);
+        return 1;
+    }
+
+    // Получить имя файла из аргументов
+    const std::string source_file_name(argv[1]);
+
+    // Открыть файл для чтения - ifstream 
+    std::ifstream input;
+    input.open(source_file_name);
+    if (!input.is_open())
+    {
+        std::cerr << "Incorrect source file name!\n";
+        return 1;
+    }
+
+    // Открыть файл для записи - ofstream 
+    std::ofstream output(argv[2], std::ios_base::app); // open
+    if (!output.is_open())
+    {
+        std::cerr << "Failed to open destination file " << argv[2] << std::endl;
+        return 1;
+    }
+
+    // перенос данных и отзеркаливание   
+    for (std::string line; std::getline(input, line); )
+    {
+        output << Flip_line(line) << std::endl;
+    }
+}
+
+void Help(const char* programName)
+{
+    std::cout << "Usage: " << programName << " <SOURCE_FILENAME> <DESTINATION_FILENAME> \n"
+        "<SOURCE_FILENAME> - the path to the file to copy data their \n"
+        "<DESTINATION_FILENAME> - the path to the file to put data from the SOURCE FILENAME" << std::endl;
+}
+
+std::string Flip_line(const std::string line)
+{
+    std::string buffer;
+    
+    if (line[0] == '0' || '1')
+    {
+        for (int i = 0; i < line.size(); ++i)
+        {
+
+            buffer += line[line.size() - i];
+        }
+        return buffer;
+    }
+    else
+    {
+        return line;
+    }
+}
