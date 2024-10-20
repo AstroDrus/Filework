d605ea4c9db6b265e39b86b8d72052cdd6dc568d
Andrew <Andevil95@mail.ru>
2024-10-20 17:51:25 +0300

    I added .vscode into .gitignore. Modified at the lesson CmakeLists.txt and Mirror.cpp

--- Mirror/Mirror.cpp
+++ Mirror/Mirror.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include "Checker.h"
 
 class Task
 {
@@ -83,8 +84,7 @@ public:
 
 
 std::unique_ptr<Task> MakeTask(int argc, char** argv);
-bool CheckFormat(char* SourceFile);
-bool CheckExpansion(const char* SourceFile);
+
 
 int main(int argc, char** argv)
 {
@@ -98,90 +98,6 @@ int main(int argc, char** argv)
 	std::unique_ptr<Task> task = MakeTask(argc, argv);
 	task->Exec();
 
-}
-
-bool CheckFormat(char* SourceFile)
-{
-	const unsigned BMPmaxsize = 65535;
-	std::ifstream input(SourceFile);
-	if (!input.is_open())
-	{
-		std::cerr << "Incorrect source file name!\n";
-		return false;
-	}
-
-	std::string colsnum;
-	std::string rowsnum;
-	bool flag = 0;
-
-	int checkparametrs = 0;
-	int checkhelper = 0;
-
-	for (std::string line; std::getline(input, line); ++checkhelper)
-	{
-
-		if (checkhelper == 1 && flag == 0)
-		{
-			for (int i = 0; i < line.size(); ++i)
-			{
-				if (line[i] == ' ')
-				{
-					flag = true;
-				}
-				else if (flag == 0)
-				{
-					colsnum += line[i];
-				}
-				else if (flag == 1)
-				{
-					rowsnum += line[i];
-				}
-				else
-				{
-					return false;
-				}
-			}
-		}
-
-		if (line[0] == '#')
-		{
-			--checkhelper;
-		}
-		else if (checkhelper == 0 && line == "P1")
-		{
-			++checkparametrs;
-		}
-		else if (checkhelper == 1 && stoi(colsnum) <= BMPmaxsize && stoi(rowsnum) <= BMPmaxsize)
-		{
-			++checkparametrs;
-			break;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	if (checkparametrs == 2)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-
-}
-
-bool CheckExpansion(const char* SourceFile)
-{
-	const std::string expectedExtension = ".bpm";
-	auto idxLastExtOccur = std::string(SourceFile).rfind(expectedExtension);
-	if (idxLastExtOccur == std::string::npos)
-	{
-		return false;
-	}
-	return (std::string(SourceFile).size() - idxLastExtOccur) == expectedExtension.size();
 }
 
 std::unique_ptr<Task> MakeTask(int argc, char** argv)
